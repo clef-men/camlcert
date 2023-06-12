9678ce93cbe93fe248eeff690f91317250b1888c
clallain <clement.allain@inria.fr>
2023-06-12 22:16:32 +0200

    Correction.

--- theories/program_logic/refinement.v
+++ theories/program_logic/refinement.v
@@ -9,13 +9,13 @@ Section behaviour.
   Context {Λ : language}.
 
   Inductive behaviour :=
-    | behaviour_converges : expr Λ → behaviour
+    | behaviour_converges : expr Λ → state Λ → behaviour
     | behaviour_diverges : behaviour.
 
   Inductive has_behaviour prog e σ : behaviour → Prop :=
     | has_behaviour_converges e' σ' :
         converges prog e σ e' σ' →
-        has_behaviour prog e σ (behaviour_converges e')
+        has_behaviour prog e σ (behaviour_converges e' σ')
     | has_behaviour_diverges :
         diverges prog e σ →
         has_behaviour prog e σ behaviour_diverges.
@@ -26,17 +26,23 @@ Section refinement.
   Context `{!Similar (val Λₛ) (val Λₜ)}.
 
   Inductive behaviour_refinement progₛ progₜ : behaviour Λₛ → behaviour Λₜ → Prop :=
-    | behaviour_refinement_val eₛ vₛ eₜ vₜ :
+    | behaviour_refinement_val eₛ vₛ σₛ eₜ vₜ σₜ:
         eₛ = of_val vₛ →
         eₜ = of_val vₜ →
         vₛ ≈ vₜ →
-        behaviour_refinement progₛ progₜ (behaviour_converges eₛ) (behaviour_converges eₜ)
-    | behaviour_refinement_stuck eₛ eₜ :
-        strongly_stuck progₛ eₛ →
-        strongly_stuck progₜ eₜ →
-        behaviour_refinement progₛ progₜ (behaviour_converges eₛ) (behaviour_converges eₜ)
+        behaviour_refinement progₛ progₜ
+          (behaviour_converges eₛ σₛ)
+          (behaviour_converges eₜ σₜ)
+    | behaviour_refinement_stuck eₛ σₛ eₜ σₜ :
+        stuck progₛ eₛ σₛ →
+        stuck progₜ eₜ σₜ →
+        behaviour_refinement progₛ progₜ
+          (behaviour_converges eₛ σₛ)
+          (behaviour_converges eₜ σₜ)
     | behaviour_refinement_diverges :
-        behaviour_refinement progₛ progₜ behaviour_diverges behaviour_diverges.
+        behaviour_refinement progₛ progₜ
+          behaviour_diverges
+          behaviour_diverges.
 
   Definition config_refinement progₛ progₜ eₛ σₛ eₜ σₜ :=
     ∀ bₜ, has_behaviour progₜ eₜ σₜ bₜ →
