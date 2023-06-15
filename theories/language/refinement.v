aa840b8e0e97a6eb701e01554b488cf1d36ba976
clallain <clement.allain@inria.fr>
2023-06-15 09:12:52 +0200

    Add language/refinement.

---
+++ theories/language/refinement.v
@@ -0,0 +1,15 @@
+From simuliris Require Import
+  prelude.
+From simuliris.program_logic Require Import
+  refinement.
+From simuliris.language Require Export
+  language.
+From simuliris.language Require Import
+  notations.
+
+Definition program_refinement (progₛ progₜ : program) :=
+  map_Forall (λ (func : function) _,
+    ∀ vₛ vₜ : val,
+    vₛ ≈ vₜ →
+    expr_refinement progₛ progₜ (func vₛ) (func vₜ)
+  ) progₛ.
