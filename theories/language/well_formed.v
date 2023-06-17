78367eecaa25c83e3db6e7b29c68cf12540e4e69
clallain <clement.allain@inria.fr>
2023-06-17 20:08:43 +0200

    Add some lemmas.

--- theories/language/well_formed.v
+++ theories/language/well_formed.v
@@ -56,3 +56,48 @@ Definition expr_well_formed' prog e :=
 Definition program_well_formed prog :=
   map_Forall (λ _, expr_well_formed prog 0) prog.
 
+Lemma expr_well_formed_subst ς1 ς2 prog lvl e :
+  (∀ x, x ≤ lvl → ς1 x = ς2 x) →
+  expr_well_formed prog lvl e →
+  e.[ς1] = e.[ς2].
+Proof.
+  revert ς1 ς2 lvl. induction e as
+    [ v
+    | x
+    | e1 IHe1 e2 IHe2
+    | e1 IHe1 e2 IHe2
+    | e IHe
+    | e1 IHe1 e2 IHe2
+    | e0 IHe0 e1 IHe1 e2 IHe2
+    | e1 IHe1 e2 IHe2
+    | e1 IHe1 e2 IHe2
+    | e1 IHe1 e2 IHe2
+    | e1 IHe1 e2 IHe2 e3 IHe3
+    ];
+    intros ς1 ς2 lvl Hς Hwf;
+    asimpl;
+    try (f_equal; naive_solver).
+  f_equal; first naive_solver.
+  eapply IHe2; last naive_solver.
+  intros x Hx. destruct x; first done. asimpl. f_equal. naive_solver lia.
+Qed.
+Lemma expr_well_formed_0_subst ς1 ς2 prog e :
+  ς1 0 = ς2 0 →
+  expr_well_formed prog 0 e →
+  e.[ς1] = e.[ς2].
+Proof.
+  intros Hς Hwf.
+  eapply expr_well_formed_subst; last done.
+  setoid_rewrite Nat.le_0_r. naive_solver.
+Qed.
+
+Lemma program_well_formed_subst ς1 ς2 prog func e :
+  ς1 0 = ς2 0 →
+  program_well_formed prog →
+  prog !! func = Some e →
+  e.[ς1] = e.[ς2].
+Proof.
+  intros Hσ Hwf Hlookup.
+  eapply expr_well_formed_0_subst; first done.
+  pose proof (map_Forall_lookup_1 _ _ _ _ Hwf Hlookup). eauto.
+Qed.
