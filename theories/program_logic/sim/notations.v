1a71fff2c8bef780f14f5d6755613d0d56dfcb8d
clallain <clement.allain@inria.fr>
2023-06-17 20:31:30 +0200

    Redesign notations.

--- theories/program_logic/sim/notations.v
+++ theories/program_logic/sim/notations.v
@@ -6,22 +6,22 @@ From simuliris.program_logic Require Export
 Notation "'SIM' eₛ ≳ eₜ [[ X ] ] {{ Φ } }" := (sim X Φ%I eₛ eₜ)
 ( at level 20,
   eₛ, eₜ, X, Φ at level 200,
-  format "'[hv' SIM  eₛ  ≳  eₜ  [[  X  ] ]  {{  '/  ' '[' Φ ']'  '/' } } ']'"
+  format "'[hv' SIM  '/  ' '[' eₛ ']'  '/' ≳  '[' eₜ ']'  '/' [[  '/  ' '[' X ']'  '/' ] ]  {{  '/  ' '[' Φ ']'  '/' } } ']'"
 ) : bi_scope.
 Notation "'SIM' eₛ ≳ eₜ {{ Φ } }" := (sim ⊥ Φ%I eₛ eₜ)
 ( at level 20,
   eₛ, eₜ, Φ at level 200,
-  format "'[hv' SIM  eₛ  ≳  eₜ  {{  '/  ' '[' Φ ']'  '/' } } ']'"
+  format "'[hv' SIM  '/  ' '[' eₛ ']'  '/' ≳  '[' eₜ ']'  '/' {{  '/  ' '[' Φ ']'  '/' } } ']'"
 ) : bi_scope.
 Notation "'SIM' eₛ ≳ eₜ [[ X ] ] [[ Φ ] ]" := (simv X Φ%I eₛ eₜ)
 ( at level 20,
   eₛ, eₜ, X, Φ at level 200,
-  format "'[hv' SIM  eₛ  ≳  eₜ  [[  X  ] ]  [[  '/  ' '[' Φ ']'  '/' ] ] ']'"
+  format "'[hv' SIM  '/  ' '[' eₛ ']'  '/' ≳  '[' eₜ ']'  '/' [[  '/  ' '[' X ']'  '/' ] ]  [[  '/  ' '[' Φ ']'  '/' ] ] ']'"
 ) : bi_scope.
 Notation "'SIM' eₛ ≳ eₜ [[ Φ ] ]" := (simv ⊥ Φ%I eₛ eₜ)
 ( at level 20,
   eₛ, eₜ, Φ at level 200,
-  format "'[hv' SIM  eₛ  ≳  eₜ  [[  '/  ' '[' Φ ']'  '/' ] ] ']'"
+  format "'[hv' SIM  '/  ' '[' eₛ ']'  '/' ≳  '[' eₜ ']'  '/' [[  '/  ' '[' Φ ']'  '/' ] ] ']'"
 ) : bi_scope.
 
 Notation "'SIM' eₛ ≳ eₜ [[ X ] ] {{ Φ } }" := (⊢ SIM eₛ ≳ eₜ [[ X ]] {{ Φ }})
