f88f9d0f1844aa6d15ec7229e58d29c4fa767ee0
clallain <clement.allain@inria.fr>
2023-06-19 21:21:36 +0200

    Correction.

--- theories/language/refinement.v
+++ theories/language/refinement.v
@@ -3,13 +3,15 @@ From simuliris Require Import
 From simuliris.program_logic Require Export
   refinement.
 From simuliris.language Require Export
-  language.
+  language
+  well_formed.
 From simuliris.language Require Import
   notations.
 
 Definition program_refinement (progₛ progₜ : program) :=
   map_Forall (λ (func : function) _,
     ∀ vₛ vₜ : val,
+    val_well_formed progₛ vₛ →
     vₛ ≈ vₜ →
     expr_refinement progₛ progₜ (func vₛ) (func vₜ)
   ) progₛ.
